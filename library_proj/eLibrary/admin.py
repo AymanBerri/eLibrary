6f0bc373fa90b9d7718fbe5829154009c417c375
unknown <berriayman11@gmail.com>
2023-05-27 19:37:18 +0300

    fixed the watchlist

--- library_proj/eLibrary/admin.py
+++ library_proj/eLibrary/admin.py
@@ -3,8 +3,9 @@ from .models import User, Book, Profile
 
 # Register your models here.
 class ProfileAdmin(admin.ModelAdmin):
-
+    list_display = ('user', )
     filter_horizontal= ('my_books',)
+    search_fields = ('user__username',)
 
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Book)
